6d43f4f050a2d3ce3efb7481da735103d4f0bc28
Nguyen Luc Khanh Lam <lc.khanhlam@gmail.com>
2021-07-12 15:37:29 +0700

    Add project files.

---
+++ PhanMemThuThapThongTinMayTinhDBS/UserControls/ucPhanMem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PhanMemThuThapThongTinMayTinhDBS.UserControls
+{
+    public partial class ucPhanMem : UserControl
+    {
+        public ucPhanMem()
+        {
+            InitializeComponent();
+        }
+    }
+}
